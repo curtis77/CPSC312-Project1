c666c7cf9d9e8838999e83bf955bfafd6c6fa072
curtis77 <curtisfox777@live.com>
2018-10-16 00:55:11 -0700

    Code update

--- explorer.hs
+++ explorer.hs
@@ -8,35 +8,47 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
 main :: IO ()
-main = play (InWindow "UI Explorer" (1000, 1000) (0,0)) blue 25 startingWorld renderWorld handleKeys stepWorld
+main = play (InWindow "UI Explorer" (800, 800) (0,0)) blue 20 startingWorld renderWorld handleKeys stepWorld
 
-data Game = ContinueGame Player
-          | EndGame
+data Game = ContinueGame Player TextInput
           deriving (Eq,Show)
- 
+
+data TextInput = TextInput String
+          deriving (Eq,Show)
+
 data Player = Player Position
-              deriving (Eq,Show)
+          deriving (Eq,Show)
 
 type Position = (Float, Float)
 
-startingWorld = (ContinueGame (Player (0,0)))
+startingWorld = (ContinueGame (Player (0,0)) (TextInput ""))
 
-renderWorld (ContinueGame (Player (x,y))) = pictures [translate x y (color red (circleSolid 5))]
+renderWorld (ContinueGame (Player (x,y)) (TextInput c)) = 
+    pictures [(translate x y (color red (circleSolid 5))), 
+              (scale 0.1 0.1 (translate 0 200 (text c))),
+              (rectangleWire 610 610)]
 
-handleKeys (EventKey (Char 'w') _ _ _) (ContinueGame (Player (x,y)))  
-    | y < 300 = (ContinueGame (Player (x, y + 20)))
-    | y > 300 = (ContinueGame (Player (x, 300)))
-handleKeys (EventKey (Char 'a') _ _ _) (ContinueGame (Player (x,y))) 
-    | x > -300 = (ContinueGame (Player (x - 20, y)))
-    | x < -300 = (ContinueGame (Player (-300, y)))
-handleKeys (EventKey (Char 's') _ _ _) (ContinueGame (Player (x,y))) 
-    | y > -300 = (ContinueGame (Player (x, y - 20)))
-    | y < -300 = (ContinueGame (Player (x, -300)))
-handleKeys (EventKey (Char 'd') _ _ _) (ContinueGame (Player (x,y))) 
-    | x < 300 = (ContinueGame (Player (x + 20,y)))
-    | x > 300 = (ContinueGame (Player (300,y)))
+handleKeys (EventKey (SpecialKey KeyUp) _ _ _) (ContinueGame (Player (x,y)) (TextInput c))  
+    | y < 300 = (ContinueGame (Player (x, y + 20)) (TextInput c))
+    | y > 300 = (ContinueGame (Player (x, 300)) (TextInput c))
+
+handleKeys (EventKey (SpecialKey KeyLeft) _ _ _) (ContinueGame (Player (x,y)) (TextInput c)) 
+    | x > -300 = (ContinueGame (Player (x - 20, y)) (TextInput c))
+    | x < -300 = (ContinueGame (Player (-300, y)) (TextInput c))
+
+handleKeys (EventKey (SpecialKey KeyDown) _ _ _) (ContinueGame (Player (x,y)) (TextInput c)) 
+    | y > -300 = (ContinueGame (Player (x, y - 20)) (TextInput c))
+    | y < -300 = (ContinueGame (Player (x, -300)) (TextInput c))
+
+handleKeys (EventKey (SpecialKey KeyRight) _ _ _) (ContinueGame (Player (x,y)) (TextInput c)) 
+    | x < 300 = (ContinueGame (Player (x + 20,y)) (TextInput c))
+    | x > 300 = (ContinueGame (Player (300,y)) (TextInput c))
+
+handleKeys (EventKey (Char b) _ _ _) (ContinueGame (Player (x,y)) (TextInput c)) = 
+    (ContinueGame (Player (x,y)) (TextInput (newInput b c)))
+
 handleKeys _ game = game
 
-stepWorld time (ContinueGame (Player (x,y))) = (ContinueGame (Player (x,y)))
+newInput c currInput = c : currInput
 
-
+stepWorld time (ContinueGame (Player (x,y)) (TextInput c)) = (ContinueGame (Player (x,y)) (TextInput c))
